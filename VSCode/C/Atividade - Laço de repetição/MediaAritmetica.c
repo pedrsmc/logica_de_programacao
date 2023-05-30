3c0703ed7bc571cca1efc90c55cba793a033d9e5
Pedro <128701224+pedrsmc@users.noreply.github.com>
2023-05-30 10:47:08 -0300

    Update MediaAritmetica.c

--- VSCode/C/Atividade - Laço de repetição/MediaAritmetica.c
+++ VSCode/C/Atividade - Laço de repetição/MediaAritmetica.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Escreva um algoritmo que pergunte ao usuário se deseja inserir mais uma nota, se a resposta do usuário for “NÃO”, o programa fará a média aritmética das notas informadas e mostrará a média aritmética para o usuário.
 // Obs.: Use um contador dentro do laço de repetição para contar a quantidade e iterações (loops).
 
-int main(void) {
+int main (void) {
     int i;
     float soma, media, notas[100];
-    char resposta;
+    char resposta[4];
 
     do {
         i++;
@@ -15,13 +16,15 @@ int main(void) {
         printf("Digite a %d nota: ", i);
         scanf("%f", &notas[i]);
 
+        fflush(stdin);
+
         soma += notas[i];
 
-        printf("\nDeseja digitar mais alguma nota (s/n): ");
-        scanf("%s", &resposta);
-        printf("\n..........................................\n\n");
-        
-    } while(resposta == 's');
+        printf("\nDeseja digitar mais alguma nota (sim/nao): ");
+        gets(resposta);
+        printf("..............................................\n\n");
+
+    } while (strcmp(resposta, "sim") == 0);
 
     media = soma / i;
 
