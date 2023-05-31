a77ec6d4083a77042a48d258be366dd7f74c35d9
Pedro <128701224+pedrsmc@users.noreply.github.com>
2023-05-31 11:02:11 -0300

    Update ParOrdemInversa.c

--- VSCode/C/Atividade - Vetores/ParOrdemInversa.c
+++ VSCode/C/Atividade - Vetores/ParOrdemInversa.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <locale.h>
 
-// Crie um programa que leia 6 n˙meros, armazenando em um vetor e informe quantos s„o pares e quantos s„o Ìmpares.
-// Mostre os n˙meros informados pelo usu·rio.
+// Crie um programa que leia 6 n√∫meros, armazenando em um vetor e informe quantos s√£o pares e quantos s√£o √≠mpares.
+// Mostre os n√∫meros informados pelo usu√°rio.
 
 int main (void) {
     setlocale(LC_ALL, "");
     int i, num[6];
 
     for (i = 0; i < 6; i++) {
-        printf("Digite um n˙mero par: ");
+        printf("Digite um n√∫mero par: ");
         scanf("%d", &num[i]);
     }
 
     printf("\nValores PARES na ordem inversa:\n");
 
-    for (i = 6; i >= 0; i--) {
+    for (i = 5; i >= 0; i--) {
         if (num[i] % 2 == 0) {
             printf("-> %d\n", num[i]);       
         }
