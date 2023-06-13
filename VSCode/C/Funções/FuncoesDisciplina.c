a6fe58a4ada0210396ad623bfea65dc7cf4b21a7
Pedro <128701224+pedrsmc@users.noreply.github.com>
2023-06-13 10:54:06 -0300

    Add files via upload

--- VSCode/C/Funções/FuncoesDisciplina.c
+++ VSCode/C/Funções/FuncoesDisciplina.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <locale.h>
 
+// Usando funções, crie um algoritmo que leia 3 disciplinas e 2 notas para cada uma delas. 
+// [1] - Mostrar nome da disciplina e a média; 
+// [2] - O nome da disciplina e a situação (aprovado, reprovado ou recuperação); 
+// [3] Exibir todas as informações; 
+// [0] - Sair do programa. 
+// Utilize o switch-case. 
+// Verifique cada nota, impedindo que o programa continue caso a nota informada seja maior que 10 ou menor que 0; 
+
 void todasInformacoes (char disciplina[][250], float media[]) {
     int i;
     
