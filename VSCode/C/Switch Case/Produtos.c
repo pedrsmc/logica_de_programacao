9cf72279a0d74476093d483a360aa35f17ddbd43
Pedro <128701224+pedrsmc@users.noreply.github.com>
2023-06-13 11:19:28 -0300

    Add files via upload

--- VSCode/C/Switch Case/Produtos.c
+++ VSCode/C/Switch Case/Produtos.c
@@ -2,32 +2,49 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main () {
+int main()
+{
     setlocale(LC_ALL, "");
 
-    int codigo;
+    int codigo, i;
 
-    printf("100 - Detergente.\n");
-    printf("101 - Esponja.\n");
-    printf("102 - Lã de aço.\n\n");
-    
-    printf("Digite o código: ");
-    scanf("%d", &codigo);
+    do {
+        i++;
+        system("cls");
 
-    switch (codigo) {
-    case 100:
-        printf("Preço: R$ 1,59.\n");
-        break;
-    case 101:
-        printf("Preço: R$ 4,59.\n");
-        break;
-    case 102:
-        printf("Preço: R$ 1,79.\n");
-        break;
-    default:
-        printf("Digite um código válido.\n");
-        break;
-    }
+        printf("100 - Detergente.\n");
+        printf("101 - Esponja.\n");
+        printf("102 - Lã de aço.\n");
+
+        printf("\nDigite o código: ");
+        scanf("%d", &codigo);
+
+        if (codigo != 0) {
+            printf("Quantidade do produto: ");
+            scanf("%d", &i);
+        }
+
+        switch (codigo) {
+        case 100:
+            printf("\nTotal = %.2f\n", 1.59 * i);
+            break;
+        case 101:
+            printf("\nTotal = %.2f\n", 4.59 * i);
+            break;
+        case 102:
+            printf("\nTotal = %.2f\n", 1.79 * i);
+            break;
+        case 0:
+        printf("\nSaindo...");
+            break;
+        default:
+            printf("\nDigite um código válido.\n");
+            break;
+        }
+
+        sleep(5);
+        system("cls");
+    } while (codigo != 0);
 
     return 0;
 }
