ce1c92a043bcadebe3883ea372868669363a6154
Pedro <128701224+pedrsmc@users.noreply.github.com>
2023-06-13 21:01:39 -0300

    Add files via upload

--- VSCode/C/Switch Case/Operacoes.c
+++ VSCode/C/Switch Case/Operacoes.c
@@ -2,34 +2,49 @@
 #include <stdlib.h>
 #include <locale.h>
 
+void soma () {
+    int i, soma = 0, numeros[2];
+
+    for (i = 0; i < 2; i++) {
+        printf("Digite %dº número: ", i + 1);
+        scanf("%d", &numeros[i]);
+        soma += numeros[i];
+    }
+
+    printf("\n\nResultado = %d", soma);
+}
+
 int main () {
     setlocale(LC_ALL, "");
 
-    int opcao, n[2], i;
+    int opcao;
 
-    printf("1 - Soma.\n");
-    printf("2 - SubtraÃ§Ã£o.\n");
-    printf("3 - MultiplicaÃ§Ã£o.\n");
-    printf("4 - DivisÃ£o.\n\n");
+    do {
 
-    printf("Digite o nÃºmero referente a operaÃ§Ã£o que deseja realizar: ");
-    scanf("%d", &opcao);
-
-    switch (opcao) {
-    case 1:
         system("cls");
 
-        printf("Soma:\n\n");
+        printf("0 - Sair.\n");
+        printf("1 - Soma.\n");
+        printf("2 - Subtração.\n");
+        printf("3 - Multiplicação.\n");
+        printf("4 - Divisão.\n\n");
 
-        for (i = 0; i < 2; i++) {
-            printf("Digite um nÃºmero: ");
-            scanf("%d", &n[i]);
+        printf("Digite o número referente a operação que deseja realizar: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+        case 0:
+            printf("\nSaindo...");
+            break;
+        case 1:
+            system("cls");
+            soma();
+            break;
         }
-
-        printf("\n%d + %d = %d\n", n[0], n[1], n[0] + n[1]);
-    
-        break;
-    }
+        
+        sleep(3);
+        system("cls");
+    } while (opcao != 0);
 
     return 0;
 }
