93fb3ca4f68c2f126db1957c46f0c4a597fd79ae
Pedro <128701224+pedrsmc@users.noreply.github.com>
2023-06-13 21:00:08 -0300

    Update Produtos.c

--- VSCode/C/Switch Case/Produtos.c
+++ VSCode/C/Switch Case/Produtos.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <locale.h>
 
+// Suponha que você esteja desenvolvendo um programa para o caixa da mercearia do seu bairro.
+// Esse programa deve ser capaz de somar o valor unitário de um produto do comércio recebendo como entrada apenas o código identificador dele.
+// Para verificar o bom funcionamento da funcionalidade de soma de valor através do código você decidiu realizar testes apenas com 
+// uma parte dos produtos vendidos, para isso criou a seguinte tabela:
+// Detergente - 1.59
+// Esponja - 4.59
+// Lã de aço - 1.79
+
 int main() {
     setlocale(LC_ALL, "");
 
